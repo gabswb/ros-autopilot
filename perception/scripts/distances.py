87db1f71664497205d534d556c5e7d04a0e58e8c
gabswb <gabriel.schwab2@gmail.com>
2023-11-14 14:27:39 +0100

    distances working git add .

--- perception/scripts/distances.py
+++ perception/scripts/distances.py
@@ -4,6 +4,7 @@ import os
 import sys
 import time
 import rospy
+import yaml
 
 import cv2 as cv
 import numpy as np
@@ -23,11 +24,15 @@ DISTANCE_SCALE_MIN = 0
 DISTANCE_SCALE_MAX = 160
 
 class DistanceExtractor (object):
-	def __init__(self, image_topic, camerainfo_topic, pointcloud_topic, output_folder, tf_rate=1):
-		self.image_topic = image_topic
-		self.camerainfo_topic = camerainfo_topic
-		self.pointcloud_topic = pointcloud_topic
-		self.output_folder = output_folder
+	def __init__(self, config):
+		self.config = config
+
+		self.image_topic = self.config["node"]["image-topic"]
+		self.camerainfo_topic = self.config["node"]["camerainfo-topic"]
+		self.pointcloud_topic = self.config["node"]["pointcloud-topic"]
+
+		# Object detector
+		self.object_detector = ObjectDetector(self.config)
 
 		# Initialize the topic subscribers
 		self.image_subscriber = rospy.Subscriber(self.image_topic, Image, self.callback_image)
@@ -43,19 +48,16 @@ class DistanceExtractor (object):
 		self.pointcloud_frame = None
 		self.latest_image = None
 		self.latest_pointcloud = None
-		self.lidar_to_camera = None
-		self.camera_to_lidar = None
-		self.distortion_parameters = None
+		self.distortion_parameter = None
 
-		self.sensor_to_image = None
+		rospy.loginfo("DistanceExtractor initialized")
 
-		rospy.loginfo("Everything ready")
-
-	def update_transforms(self):
+	def get_transform(self, source_frame, target_frame):
 		"""Update the lidar-to-camera transform matrix from the tf topic"""
 		try:
 			# It’s lookup_transform(target_frame, source_frame, …) !!!
-			transform = self.tf_buffer.lookup_transform(self.image_frame, self.pointcloud_frame, rospy.Time(0))
+			transform = self.tf_buffer.lookup_transform(target_frame, source_frame, rospy.Time(0))
+
 		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
 			return
 
@@ -67,10 +69,14 @@ class DistanceExtractor (object):
 		translation_vector = np.asarray((translation_message.x, translation_message.y, translation_message.z)).reshape(3, 1)
 		
 		# Build the complete transform matrix
-		self.lidar_to_camera = np.concatenate((
+		a = np.concatenate((
 			np.concatenate((rotation_matrix, translation_vector), axis=1),
 			np.asarray((0, 0, 0, 1)).reshape((1, 4))
 		), axis=0)
+		# rospy.loginfo(f'lidar_to_camera.shape {a.shape}')
+		# rospy.loginfo(f'lidar_to_camera {a}')
+		return a
+	
 
 	def callback_image(self, data):
 		"""Extract an image from the camera"""
@@ -96,82 +102,82 @@ class DistanceExtractor (object):
 	def callback_camerainfo(self, data):
 		"""Update the camera info"""
 		self.sensor_to_image = np.asarray(data.K).reshape((3, 3))
-		self.distortion_parameters = data.D[0]
+		self.distortion_parameter = data.D[0]
 
 	def convert_pointcloud(self):
 		"""Superimpose a point cloud from the lidar onto an image from the camera"""
-		# Cannot work if any piece of information is missing
-		if (self.image_frame is None or self.pointcloud_frame is None or
-			self.latest_image is None or self.latest_pointcloud is None or
-			self.sensor_to_image is None):
-			return
-		self.update_transforms()
 
-		# Easier to make a gradient in HSV
-		lidar_image = cv.cvtColor(self.latest_image, cv.COLOR_RGB2HSV)
+		image = self.latest_image
+		objects_bbox = self.object_detector.detect(image)
 
-		# HACK : Error in my code or error in the focal returned by /forwardCamera/camera_info …?
-		sensor_to_image = self.sensor_to_image.copy()
-		#camera_to_image[0, 0] *= 0.51
-		#camera_to_image[1, 1] *= 0.51
+		if len(objects_bbox) > 0:
+			pointcloud = self.latest_pointcloud
+			sensor_to_image = self.sensor_to_image
+			lidar_to_camera = self.get_transform(self.pointcloud_frame, self.image_frame)
 
-		pointcloud = self.latest_pointcloud
+			# Easier to make a gradient in HSV
+			# image_to_print = cv.cvtColor(image, cv.COLOR_RGB2HSV)
 
-		pointcloud_camera = self.lidar_to_camera @ pointcloud # (4,N) = (4,4) @ (4,N)
+			pointcloud_camera = lidar_to_camera @ pointcloud # (4,N) = (4,4) @ (4,N)
 
-
-		# Filter out points that are behind the camera, otherwise the following calculations overlay them on the image with inverted coordinates
-		valid_points = (pointcloud_camera[2, :] >= 0) # (N,)
-		pointcloud_camera = pointcloud_camera[:, valid_points] # (4,N')
-		pointcloud = pointcloud[:, valid_points] # (4,N')
-
-		ro = np.linalg.norm(pointcloud_camera[:3,:], axis=0) # (N,)
-
-		pointcloud_sensor = np.asarray((
-			pointcloud_camera[0] / (pointcloud_camera[2]+(ro*self.distortion_parameters)),
-			pointcloud_camera[1] / (pointcloud_camera[2]+(ro*self.distortion_parameters)),
-			np.ones(pointcloud_camera.shape[1])
-		))#.transpose().reshape(-1, 2) (3,N)
-
-		pointcloud_image = sensor_to_image @ pointcloud_sensor # (3,N)
-
-		# Finalize the projection ([u v w] ⟶ [u/w v/w])
-		image_points = np.asarray((
-			pointcloud_image[0] / (pointcloud_image[2]),
-			pointcloud_image[1] / (pointcloud_image[2]),
-		))#.transpose() # (2,N)
-
-		# # Visualize the lidar data projection onto the image
-		# temp_img = lidar_image.copy()
-		# for i in range(image_points.shape[1]):
-		# 	if 0 <= image_points[0,i] <= temp_img.shape[1] and 0 <= image_points[1,i] <= temp_img.shape[0]:
-		# 		cv.drawMarker(temp_img, (int(image_points[0,i]), int(image_points[1,i])), (0, 255, 0), cv.MARKER_CROSS, 4)
-			
-		# cv.imshow('dist', temp_img)
-		# cv.waitKey(5)
+			# Filter out points that are behind the camera, otherwise the following calculations overlay them on the image with inverted coordinates
+			valid_points = (pointcloud_camera[2, :] >= 0) # (N,)
+			pointcloud_camera = pointcloud_camera[:, valid_points] # (4,N')
+			pointcloud = pointcloud[:, valid_points] # (4,N')
 
 
-		# # Calculate the distance to each lidar point and the associated color gradient
-		#distances = np.linalg.norm(pointcloud[:3,:], axis=0)
-		colors = self.get_color(ro)
+			ro = np.linalg.norm(pointcloud_camera[:3,:], axis=0) # (N',)
 
-		# Write all points to the final image
-		for color, point in zip(colors, image_points.T):
-			if 0 <= point[0] < lidar_image.shape[1] and 0 <= point[1] < lidar_image.shape[0]:
-				lidar_image[int(point[1]), int(point[0])] = color
-				# `+` dots
-				if point[1] > 0:
-					lidar_image[int(point[1]) - 1, int(point[0])] = color
-				if point[1] < lidar_image.shape[0] - 1:
-					lidar_image[int(point[1]) + 1, int(point[0])] = color
-				if point[0] > 0:
-					lidar_image[int(point[1]), int(point[0]) - 1] = color
-				if point[0] < lidar_image.shape[1] - 1:
-					lidar_image[int(point[1]), int(point[0]) + 1] = color
+			pointcloud_sensor = np.asarray((
+				pointcloud_camera[0] / (pointcloud_camera[2]+(ro*self.distortion_parameter)),
+				pointcloud_camera[1] / (pointcloud_camera[2]+(ro*self.distortion_parameter)),
+				np.ones(pointcloud_camera.shape[1])
+			))
 
-		rgb_output_image = cv.cvtColor(lidar_image, cv.COLOR_HSV2BGR)
-		cv.imshow('dist', rgb_output_image)
-		cv.waitKey(5)
+			pointcloud_image = sensor_to_image @ pointcloud_sensor # (3,N')
+
+			# Finalize the projection ([u v w] ⟶ [u/w v/w])
+			image_points = np.asarray((
+				pointcloud_image[0] / (pointcloud_image[2]),
+				pointcloud_image[1] / (pointcloud_image[2]),
+			)) # (2,N')
+
+			for bbox in objects_bbox:
+				bbox_cropped= bbox
+				x_crop = bbox.w//3
+				y_crop = bbox.h//3
+				bbox_cropped.x += x_crop
+				bbox_cropped.y += y_crop
+				bbox_cropped.w -= x_crop*2
+				bbox_cropped.h -= y_crop*2
+				filter = ((bbox_cropped.x <= image_points[0]) & (image_points[0] <= bbox_cropped.x + bbox_cropped.w) &
+			  			  (bbox_cropped.y <= image_points[1]) & (image_points[1] <= bbox_cropped.y + bbox_cropped.h))
+				relevant_points_image = image_points[:, filter] # (2,N'')
+				relevant_points_camera = pointcloud_camera[:, filter] #(4,N'')
+
+				# distances = ro[filter]
+				# distances in the camera frame 
+				distances = np.linalg.norm(relevant_points_camera[:3,:], axis=0) #(N'',)
+				#colors = self.get_color(distances)
+
+				index = np.argsort(distances)[len(distances)//2]
+				dist = distances[index]
+
+				self.object_detector.draw_bounding_box(image, bbox.class_id, 0.5, bbox.x, bbox.y, bbox.x+bbox.w, bbox.y+bbox.h)
+				image = cv.putText(image, f'd = {dist} m', (bbox.x, bbox.y-25), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+			cv.imshow('dist', image)
+			cv.waitKey(5)
+
+			# output_img = image.copy()
+			# # Write all points to the final image
+			# for color, point in zip(colors, image_points.T):
+			# 	if 0 <= point[0] < output_img.shape[1] and 0 <= point[1] < output_img.shape[0]:
+			# 			cv.drawMarker(output_img, (int(point[0]), int(point[1])), color, cv.MARKER_CROSS, 4)
+
+
+			# # rgb_output_image = cv.cvtColor(image_to_print, cv.COLOR_HSV2BGR)
+			# cv.imshow('dist', output_img)
+			# cv.waitKey(5)
 
 	def get_color(self, distances):
 		# Generate the color gradient from point distances
@@ -182,10 +188,13 @@ class DistanceExtractor (object):
 		)).astype(np.uint8).transpose()
 
 
-	
-
 if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print(f"Usage : {sys.argv[0]} <config-file>")
+	else:
+		with open(sys.argv[1], "r") as config_file:
+			config = yaml.load(config_file, yaml.Loader)
 	rospy.init_node("distances")
-	node = DistanceExtractor("/forwardCamera/image_raw", "/forwardCamera/camera_info", "/lidar", '$HOME/Documents')
+	node = DistanceExtractor(config)
 	rospy.spin()
 
